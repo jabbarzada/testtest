162616da3c82f0fbafd54caf0281ca1628c4d632
Jabbarzada <elnurjabbarzada@gmail.com>
2025-08-29 22:04:40 +0400

    ilk commit

---
+++ testt.py
@@ -0,0 +1,5 @@
+git init                       # Git başlat
+git remote add origin https://github.com/senin-adin/my-first-project.git
+git add .                      # bütün faylları əlavə et
+git commit -m "ilk commit"     # qeyd et
+git push -u origin master      # GitHub-a göndər
